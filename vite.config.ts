8e01c88a3c66fe31ccb4f6e9207dd1680f80d861
Vino Tanuwijaya <vino8959@gmail.com>
2026-09-21 18:07:40 +0700

    Set base path for invoice generator

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/invoice-generator/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
